ae180790afea59fd5c0a53baf8783a868fdbe7e0
Aykut Yüksel <aykut38080@gmail.com>
2023-03-29 00:14:34 +0300

    For practics

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using DapperEdu.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperEdu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductRepository productRepository;
+
+        public ProductController()
+        {
+            productRepository= new ProductRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return productRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return productRepository.GetById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Product prod)
+        {
+            if (ModelState.IsValid)
+                productRepository.Add(prod);      
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product prod)
+        {
+            prod.ProductId= id;
+            if (ModelState.IsValid) 
+             productRepository.Update(prod); 
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            productRepository.Delete(id);
+        }
+
+
+
+    }
+}
